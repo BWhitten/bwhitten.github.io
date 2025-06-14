247339c17e3603fa489e7b67b8d24a83f09201c0
Ben Whitten <ben.whitten@gmail.com>
2025-06-14 22:43:09 +0100

    add sample images and layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
+var msnry;
+
 window.addEventListener('load', (event) => {
   console.log("Page loaded.");
+
+  msnry = new Masonry( '.grid', {
+  // set itemSelector so .grid-sizer is not used in layout
+  itemSelector: '.grid-item',
+  // use element for option
+  columnWidth: '.grid-sizer',
+  gutter: '.gutter-sizer',
+  percentPosition: true,
+  });
+
+});
+
+
+imagesLoaded('.grid', function( instance ) {
+  console.log('all images are loaded');
+  msnry.layout()
 });
