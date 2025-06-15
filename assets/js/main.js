73fed1afc2d0507f50c3ef0ac66b9938bfc46922
Ben Whitten <ben.whitten@gmail.com>
2025-06-15 14:38:41 +0100

    add image click modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,4 +21,28 @@ window.addEventListener('load', (event) => {
     msnry.layout();
   });
 
+  // Get the modal
+  var modal = document.getElementById("displaymodal");
+
+  // Get the images and insert it inside the modal when clicked
+  var modalImg = document.getElementById("img01");
+  
+  const imgList = document.querySelectorAll(".grid-item > img");
+  imgList.forEach(img => {
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.src = this.src;
+      }
+  });
+  
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() { 
+    modal.style.display = "none";
+  }
+
 });
+
