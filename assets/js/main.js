e2f77e0eaa33cccfc72794d9a006f36135112509
Ben Whitten <ben.whitten@gmail.com>
2025-06-15 10:51:16 +0100

    layout masonry when images load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,24 @@
-var msnry;
-
 window.addEventListener('load', (event) => {
   console.log("Page loaded.");
 
-  msnry = new Masonry( '.grid', {
-  // set itemSelector so .grid-sizer is not used in layout
-  itemSelector: '.grid-item',
-  // use element for option
-  columnWidth: '.grid-sizer',
-  gutter: '.gutter-sizer',
-  percentPosition: true,
+
+  // init Masonry
+  var grid = document.querySelector('.grid');
+
+
+  var msnry = new Masonry( grid, {
+    // set itemSelector so .grid-sizer is not used in layout
+    itemSelector: '.grid-item',
+    // use element for option
+    columnWidth: '.grid-sizer',
+    gutter: '.gutter-sizer',
+    percentPosition: true,
   });
 
-});
+  imagesLoaded( grid ).on( 'progress', function() {
+    // layout Masonry after each image loads
+    console.log("Image loaded.");
+    msnry.layout();
+  });
 
-
-imagesLoaded('.grid', function( instance ) {
-  console.log('all images are loaded');
-  msnry.layout()
 });
